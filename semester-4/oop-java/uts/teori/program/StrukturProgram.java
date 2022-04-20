76c2c6bb8193be6fcb4b04a64a259d602cb5fb51
Farhan <kuuhaku.1332@gmail.com>
2022-04-20 10:22:53 +0700

    tambahan

--- semester-4/oop-java/uts/teori/program/StrukturProgram.java
+++ semester-4/oop-java/uts/teori/program/StrukturProgram.java
@@ -1,3 +1,4 @@
+// Nomor 1
 // Deklarasi Package adalah pemanggilan folder dimana file ini berada
 package program;
 
@@ -11,6 +12,7 @@ public class StrukturProgram {
 
     // Method main adalah method yang akan dijalankan pertama kali oleh sistem.
     public static void main(String[] args) {
+        // Nomor 2
         // variabel dengan nama nilaiTugas yang memiliki tipe data non primitif String
         // yang memiliki nilai "95";
         String nilaiTugas = "95";
