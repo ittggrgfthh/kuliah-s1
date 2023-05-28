c4b8a08ba4e51b35255946244e41b5a7778d9564
Farhan <kuuhaku.1332@gmail.com>
2023-05-28 16:31:10 +0700

    add: aplikasi data obat

--- semester-6/aplikasi-mobile/DataObat/app/src/main/java/com/example/dataobat/MainActivity.java
+++ semester-6/aplikasi-mobile/DataObat/app/src/main/java/com/example/dataobat/MainActivity.java
@@ -24,10 +24,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         dbh = new DatabaseHelper(this);
-
         recyclerObat = findViewById(R.id.recyclerViewObat);
-
 
         Cursor cursor = dbh.ReadData();
 
